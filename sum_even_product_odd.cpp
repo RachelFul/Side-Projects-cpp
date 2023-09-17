8b3f3079317451ceef71f0de7977c9face448bb2
Rachel <144191402+RachelFul@users.noreply.github.com>
2023-09-17 17:16:14 +0700

    Create sum_even_product_odd.cpp

---
+++ sum_even_product_odd.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+  int total_even = 0;
+  int product_odd = 1;
+
+  vector<int> vector = {2, 4, 3, 6, 1, 9};
+
+  for (int i = 0; i < vector.size(); i++) {
+    if (vector[i] % 2 == 0) {
+      total_even = total_even + vector[i];
+    }
+
+    else {
+      product_odd = product_odd * vector[i];
+    }
+  }
+
+  cout << "Sum of even: " << total_even << "\n";
+  cout << "Product of odd: " << product_odd << "\n";
+
+}
